c47a77929e5e97f8a7a5516ce0315dbd8af38d19
sandscorpio <amit@mangobird.com>
2015-04-22 20:55:56 -0700

    move constants here

---
+++ constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+SENDGRID_USERNAME = 'sandscorpio'
+SENDGRID_PASSWORD = 'Sendgrid-00'
+
+MAILGUN_KEY = 'key-6xnrfd7a38uqa56nfq35ocq4nrqyiis1'
+MAILGUN_DOMAIN = 'https://api.mailgun.net/v3/mangobird.mailgun.org/messages'
